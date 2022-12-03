cb1c464c5ef03681074fb63646b1aa8c1f503641
Alexander von Renteln <alexander.vonrentlen@advantest.com>
2022-12-04 00:11:58 +0100

    add kotlin stuff :-)

--- 03/kotlin/src/main/kotlin/Main.kt
+++ 03/kotlin/src/main/kotlin/Main.kt
@@ -1,7 +1,28 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.io.File
+import java.lang.Exception
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    val keys = ('a'..'z') + ('A'..'Z')
+    val values = 1..52
+    val lookup = keys.zip(values).toMap()
+
+    val lines = getLinesFromFile("../demo-input.txt")
+
+    val result = lines.map { line ->
+        val size = line.length / 2
+        val a = line.substring(0, size).toSet()
+        val b = line.substring(size).toSet()
+        val inter = a.intersect(b)
+        assert(inter.size == 1)
+        lookup.getValue(inter.first())
+    }.sum()
+
+    println(result)
 }
+
+
+fun printLineByLine(fileName: String)
+        = File(fileName).forEachLine { println(it) }
+
+fun getLinesFromFile(fileName: String): List<String>
+        = File(fileName).useLines { it.toList() }
